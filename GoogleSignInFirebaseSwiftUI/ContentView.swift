dc153341903b8302f9d88d25314d7575a4bdb712
sheehanmunim <sheehanmunim@gmail.com>
2024-06-09 20:01:49 -0400

    Initial Commit

---
+++ GoogleSignInFirebaseSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoogleSignInFirebaseSwiftUI
+//
+//  Created by Sheehan Munim on 6/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoogleSignInFirebaseSwiftUI/GoogleSignInFirebaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInFirebaseSwiftUIApp.swift
+//  GoogleSignInFirebaseSwiftUI
+//
+//  Created by Sheehan Munim on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSignInFirebaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
