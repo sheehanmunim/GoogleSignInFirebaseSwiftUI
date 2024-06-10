92ae78c1d354ede412bedabdc44a70fe16742a54
sheehanmunim <sheehanmunim@gmail.com>
2024-06-09 20:46:55 -0400

    Google Sign In Using Firebase SwiftUI

---
+++ GoogleSignInFirebaseSwiftUI/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  GoogleSignInFirebaseSwiftUI
+//
+//  Created by Sheehan Munim on 6/9/24.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
---
+++ GoogleSignInFirebaseSwiftUI/InitialView.swift
@@ -0,0 +1,18 @@
+//
+//  InitialView.swift
+//  GoogleSignInFirebaseSwiftUI
+//
+//  Created by Sheehan Munim on 6/9/24.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialView()
+}
